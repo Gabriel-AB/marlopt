b9bc1a55e11e4ada8710649fbf6a7eda1774695f
Gabriel Barbosa <gabriel.a07b@gmail.com>
2021-12-17 14:36:39 -0300

    Adding environments module to package

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ setup(
     name='marlopt',
     version='0.0.1',
     author='Gabriel-AB',
-    packages=['marlopt'],
+    packages=[
+        'marlopt', 
+        'marlopt.environments'
+    ],
     install_requires=[
         'optfuncs @ git+https://github.com/Gabriel-AB/optfuncs@packaging',
         'pettingzoo',
